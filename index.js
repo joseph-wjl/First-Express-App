ace38e015cb16bc1e43d6a97c801423466823260
Joseph Lau <lau.joseph.ca@gmail.com>
2025-05-21 01:33:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express()
 
-app.use(() => {
+app.use((req, res) => {
     console.log("we got a new request")
+    res.send('<h1>This is my homepage!</h1>')
 })
 
 app.listen(3000, () => {
