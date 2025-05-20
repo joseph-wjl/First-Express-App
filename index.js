e3a4f270fe03e8bbd1c4ceb299a30a392b3a523b
Joseph Lau <lau.joseph.ca@gmail.com>
2025-05-21 01:49:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express()
 
-app.use((req, res) => {
-    console.log("we got a new request")
-    res.send('<h1>This is my homepage!</h1>')
+// app.use((req, res) => {
+//     console.log("we got a new request")
+//     // res.send('<h1>This is my homepage!</h1>')
+// })
+
+app.get('/cats', (req, res) => {
+    res.send('Meow!')
 })
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
+
